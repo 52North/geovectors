e771088b25fffbba35994227554910576b74a940
Martin Pontius <m.pontius@52north.org>
2026-02-24 16:46:43 +0100

    Update installation and remove pkg_resources

--- geovectorslib/__init__.py
+++ geovectorslib/__init__.py
@@ -1,11 +1,4 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .geod import direct, inverse
 
-
-try:
-    __version__ = get_distribution('GeoVectors').version
-except DistributionNotFound:
-    __version__ = '(local)'
 
 __all__ = ['direct', 'inverse']
